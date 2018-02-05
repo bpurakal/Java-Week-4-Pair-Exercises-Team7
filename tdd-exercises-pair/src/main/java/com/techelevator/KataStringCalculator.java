aa7fab5ddc501e0de77e8ca5ab3e0df79521f7ef
Bryan Purakal <bpurakal@gmail.com>
2018-02-05 14:50:00 -0500

    junk

--- tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -2,4 +2,11 @@ package com.techelevator;
 
 public class KataStringCalculator {
 
+	public static int add(String numbers) {
+		int sum = 0;
+		if (numbers == "") {
+			return sum;
+		}
+		return null
+	}
 }
--- tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -4,5 +4,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
-
+	
+	@Test
+	public void testIfStringIsEmpty() {
+		assertEquals(0, KataStringCalculator.add(""));
+		
+	}
 }
