9cf4e70ec9bfbc79b7bb9d3c6e989a9393d53b9f
Eran KaranouhSchuler <ekaranou@kent.edu>
2018-02-05 14:48:13 -0500

    Finished Team 7 TDD Part 1 Pair Exercises

--- tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -2,4 +2,31 @@ package com.techelevator;
 
 public class KataStringCalculator {
 
+	public static int add(String  numbers) {
+		int sum = 0;
+		
+		if(numbers.equals("")) {
+			
+		} else if(numbers.startsWith("//")) {
+			String delimiter = numbers.substring(2,3);
+			String numbersWithoutSetup = numbers.substring(4);
+			sum = sumCrazyShit(delimiter, numbersWithoutSetup);
+		} else {
+			String numbersWithoutNewLines = numbers.replace("\n", ",");
+			sum = sumCrazyShit(",", numbersWithoutNewLines);
+		}
+		return sum;
+	}
+
+	private static int sumCrazyShit(String delimiter, String numbersWithoutSetup) {
+		int sum = 0;
+		String[] arrayOfNumbers = numbersWithoutSetup.split(delimiter);
+		for(String element : arrayOfNumbers) {
+			int parsedNumbersString = Integer.parseInt(element);
+			sum += parsedNumbersString;
+		}
+		return sum;
+	}
+	
+
 }
--- tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -4,5 +4,45 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
+	
+	@Test
+	public void testIfStringIsEmpty() {
+		assertEquals(0, KataStringCalculator.add(""));
+	}
+	
+	@Test
+	public void testIfStringIsOne() {
+		assertEquals(1, KataStringCalculator.add("1"));
+	}
+	
+	@Test
+	public void testIfStringIsTwo() {
+		assertEquals(2, KataStringCalculator.add("2"));
+	}
+	
+	@Test
+	public void testIfCalculatedStringIsThree() {
+		assertEquals(3, KataStringCalculator.add("1,2"));
+	}
+	
+	@Test
+	public void testIfCalculatedStringIsFiftySix() {
+		assertEquals(56, KataStringCalculator.add("49,7"));
+	}
+	
+	@Test
+	public void testIfTripleCalculatedStringIsFiftySix() {
+		assertEquals(56, KataStringCalculator.add("45,7,4"));
+	}
+	
+	@Test
+	public void testIfTripleCalculatedStringIsSix() {
+		assertEquals(6, KataStringCalculator.add("4,1\n1"));
+	}
+	
+	@Test
+	public void testIfDifferentDelimiter() {
+		assertEquals(9, KataStringCalculator.add("//;\n1;8"));
+	}
 
 }
